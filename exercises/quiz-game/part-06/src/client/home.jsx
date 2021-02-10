57ca67723735d609255baff537750067c5e39c71
Kristoffer Larsen Hopland <24739853+97krihop@users.noreply.github.com>
2021-02-10 17:59:54 +0100

    task 6 done

---
+++ exercises/quiz-game/part-06/src/client/home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+
+export function Home() {
+    return (<div>
+        <h1>hello welcome to quiz game</h1>
+        <Link to={"/match"}>
+            <button>start game</button>
+        </Link>
+    </div>)
+}
---
+++ exercises/quiz-game/part-06/tests/quizzes-test.js
@@ -0,0 +1,24 @@
+const {getRandomQuizzes} = require("../src/client/quizzes");
+
+test("invalid", ()=>{
+    expect(() => getRandomQuizzes(-1)).toThrow();
+    expect(() => getRandomQuizzes(0)).toThrow();
+    expect(() => getRandomQuizzes(100)).toThrow();
+})
+test("valid 1 ", ()=>{
+    const quiz = getRandomQuizzes(1);
+    expect(quiz.length).toBe(1);
+    expect(quiz[0].question).toBeDefined();
+    expect(quiz[0].answers).toBeDefined();
+    expect(quiz[0].answers.length).toBe(4);
+})
+
+test("Test 2", () => {
+
+    for(let i=0; i<100; i++) {
+        const quiz = getRandomQuizzes(2);
+
+        expect(quiz.length).toBe(2);
+        expect(quiz[0].question).not.toBe(quiz[1].question);
+    }
+});
