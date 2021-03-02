01a27de2eb84170f04ceb0e647cd23d4bffe0fc2
Kristoffer Larsen Hopland <24739853+97krihop@users.noreply.github.com>
2021-03-02 21:14:03 +0100

    test

--- exercises/quiz-game/part-06/src/client/match.jsx
+++ exercises/quiz-game/part-06/src/client/match.jsx
@@ -1,95 +1,111 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from "react";
 
-import { getRandomQuizzes } from './quizzes';
+import {getRandomQuizzes} from './quizzes';
+
 
 export function Match() {
+    const [error, setError] = useState(null);
     const [quiz, setQuiz] = useState();
     const [victory, setVictory] = useState(false);
     const [defeat, setDefeat] = useState(false);
     const [current, setCurrent] = useState(0);
     const [length, setLength] = useState(0);
 
+
     useEffect(() => {
-        startGame();
-    }, []);
+        startNewMatch()
+    })
 
-    const startGame = () => {
-        setQuiz(getRandomQuizzes(3));
-        setVictory(false);
-        setDefeat(false);
-        setCurrent(0);
-        setLength(3);
+
+    const startNewMatch = async () => {
+
+        //because getRandomQuizzes is async, it does return a Promise we can await on
+        const quizzes = await getRandomQuizzes(3);
+
+        this.setState(
+            !quizzes ? {error: "Error when connecting to server"}
+                : {
+                    error: null,
+                    match: {
+                        victory: false,
+                        defeat: false,
+                        quizzes: quizzes,
+                        currentIndex: 0,
+                        numberOfQuizzes: quizzes.length
+                    }
+                }
+        );
+
     };
 
-    const handleClick = (x) => {
-        if (x) {
-            if (current === length - 1) setVictory(true);
-            else setCurrent(current + 1);
-        } else setDefeat(true);
+    const handleClick = (correct) => {
+        if (correct) {
+            if (this.state.match.currentIndex === (this.state.match.numberOfQuizzes - 1)) {
+                //last quiz
+                this.setState({match: {victory: true}});
+            } else {
+                //go on to next quiz
+                this.setState(prev => ({
+                    match: {
+                        currentIndex: prev.match.currentIndex + 1,
+                        quizzes: prev.match.quizzes,
+                        numberOfQuizzes: prev.match.numberOfQuizzes
+                    }
+                }));
+            }
+
+        } else {
+            this.setState({match: {defeat: true}});
+        }
     };
 
-    const renderAnswerTag = (prefix, answer, correct) => (
-        <button className="answer" onClick={() => handleClick(correct)}>
-            {' '}
-            {prefix + answer}{' '}
-        </button>
-    );
 
-    if (!quiz) {
-        return <h2>loading ...</h2>;
+    function renderAnswerTag(prefix, answer, correct) {
+        return <div className='answer' onClick={() => this.handleClick(correct)} tabindex="0"> {prefix + answer} </div>;
     }
 
-    if (victory) {
+    if (error) {
+        return <h2>{error}</h2>
+    }
+
+    if (!this.state.match) {
+        return <h2>Loading...</h2>;
+    }
+
+    if (this.state.match.victory) {
         return (
-            <div>
+            <div className="game-result">
                 <h2>You Won!</h2>
-                <div>
-                    <button className="quiz" onClick={startGame}>
-                        New Match
-                    </button>
+                <div className="action">
+                    <button className="play new-game-button" onClick={this.startNewMatch}>New Match</button>
                 </div>
             </div>
         );
     }
 
-    if (defeat) {
+    if (this.state.match.defeat) {
         return (
-            <div>
+            <div className="game-result">
                 <h2>Wrong Answer! You Lost!</h2>
-                <div>
-                    <button className={'quiz'} onClick={startGame}>
-                        New Match
-                    </button>
+                <div className="action">
+                    <button className="play new-game-button" onClick={this.startNewMatch}>New Match</button>
                 </div>
             </div>
         );
     }
+
+    const match = this.state.match;
+    const count = "" + (match.currentIndex + 1) + "/" + match.numberOfQuizzes;
+    const quiz = match.quizzes[match.currentIndex];
 
     return (
-        <div>
-            <p className="question">Question: {quiz[current].question} </p>
-            <div className={'quizContainer'}>
-                {renderAnswerTag(
-                    'A: ',
-                    quiz[current].answers[0],
-                    quiz[current].correct === 0
-                )}
-                {renderAnswerTag(
-                    'B: ',
-                    quiz[current].answers[1],
-                    quiz[current].correct === 1
-                )}
-                {renderAnswerTag(
-                    'C: ',
-                    quiz[current].answers[2],
-                    quiz[current].correct === 2
-                )}
-                {renderAnswerTag(
-                    'D: ',
-                    quiz[current].answers[3],
-                    quiz[current].correct === 3
-                )}
-            </div>
+        <div id={"quiz_" + quiz.id} className="quiz">
+            <p className='question'>Question {count}: {quiz.question} </p>
+            {this.renderAnswerTag("A: ", quiz.answers[0], quiz.indexOfRightAnswer === 0)}
+            {this.renderAnswerTag("B: ", quiz.answers[1], quiz.indexOfRightAnswer === 1)}
+            {this.renderAnswerTag("C: ", quiz.answers[2], quiz.indexOfRightAnswer === 2)}
+            {this.renderAnswerTag("D: ", quiz.answers[3], quiz.indexOfRightAnswer === 3)}
         </div>
     );
+}
 }
--- exercises/quiz-game/part-06/src/client/quizzes.js
+++ exercises/quiz-game/part-06/src/client/quizzes.js
@@ -1,4 +1,3 @@
-/*
 const quiz = [{
     question: 'what is absolute zero?',
     answers: ['-300 Celsius', '0 Kelvin', '0  Fahrenheit', '0 Celsius'],
@@ -13,45 +12,39 @@ const quiz = [{
     correct: 3,
 }];
 
-export function getRandomQuizzes(numberOfQuizzes) {
-    if (numberOfQuizzes < 1) throw "Invalid number too low";
-
-    if (numberOfQuizzes > quiz.length) throw "Too many quizzes asked for";
-
-    const list = Array(numberOfQuizzes);
-
-    let i = 0;
-    while (i < numberOfQuizzes) {
-
-        const k = Math.floor(quiz.length * Math.random());
-        if (!list.includes(k)) {
-            list[i] = k;
-            i++;
-        }
-    }
-    return Array.from(list).map(x => quiz[x])
-}*/
 export async function getRandomQuizzes(numberOfQuizzes) {
-    if (numberOfQuizzes < 1) throw 'Invalid number too low';
 
-    const url = 'https://opentdb.com/api.php?type=multiple&amount=';
+    if (numberOfQuizzes < 1) {
+        throw "Invalid number of requested quizzes: " + n;
+    }
+
+
+    const url = "https://opentdb.com/api.php?type=multiple&amount=" + numberOfQuizzes;
     let response;
-    let data;
+    let payload;
+
     try {
-        response = await fetch(url + numberOfQuizzes);
-        data = await response.json();
+        response = await fetch(url);
+        payload = await response.json();
     } catch (err) {
         return null;
     }
 
-    if (response.status !== 200) return null;
-    return data.results.map((x) => {
-        const pos = Math.floor(Math.random() * Math.floor(3));
-        x.incorrect_answers.splice(pos, 0, x.correct_answer);
+    if (response.status !== 200) {
+        return null;
+    }
+
+    return payload.results.map(q => {
+
+        const correct = Math.floor(Math.random() * Math.floor(3));
+        const answers = q.incorrect_answers;
+        answers.splice(correct, 0, q.correct_answer);
+
         return {
-            question: x.question,
-            answers: x.incorrect_answers,
-            indexOfRightAnswer: pos,
+            question: q.question,
+            answers: answers,
+            indexOfRightAnswer: correct,
+            id: 0
         };
-    });
+    })
 }
